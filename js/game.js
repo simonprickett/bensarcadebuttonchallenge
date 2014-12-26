6b76b1fff22cabd08494e5f557ebac99a499ce26
Simon Prickett <simon@crudworks.org>
2014-12-26 16:42:21 -0500

    Added proper end of game reset and second animation.

--- js/game.js
+++ js/game.js
@@ -4,7 +4,17 @@ var basicGame = {
 	winningScore: 10,
 
 	init: function() {
+		var n = 0;
+
+		for (n = 1; n < 11; n++) {
+			document.getElementById('player1-' + n).className = 'meterElement';
+			document.getElementById('player2-' + n).className = 'meterElement';
+		}
+
+		basicGame.player1Pos = 0;
+		basicGame.player2Pos = 0;
 		window.onkeypress = basicGame.buttonPressed;
+
 	},
 
 	buttonPressed: function(e) {
@@ -24,9 +34,13 @@ var basicGame = {
 
 		// Did anybody win?
 		if (basicGame.player1Pos == basicGame.winningScore) {
+			document.getElementById('player1').className += ' animated flip';
 			alert('Player 1 Wins!');
+			basicGame.init();
 		} else if (basicGame.player2Pos == basicGame.winningScore) {
+			document.getElementById('player2').className += ' animated flip';
 			alert('Player 2 Wins!');
+			basicGame.init();
 		}
 
 	}
