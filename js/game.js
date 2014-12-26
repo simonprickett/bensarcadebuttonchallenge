cdebe0a9667674b063a540b3f8f1cad82be9b619
Simon Prickett <simon@crudworks.org>
2014-12-25 23:01:24 -0500

    Initial check in

---
+++ js/game.js
@@ -0,0 +1,38 @@
+// TODO
+var basicGame = {
+	player1Pos: 0,
+	player2Pos: 0,
+	winningScore: 10,
+
+	init: function() {
+		window.onkeypress = basicGame.buttonPressed;
+	},
+
+	buttonPressed: function(e) {
+		// 49 = Green button, player 1
+		// 50 = Red button, player 2
+		var elemToUpdate = undefined;
+
+		if (e.keyCode == 49) {
+			basicGame.player1Pos += 1;
+			elemToUpdate = document.getElementById('player1-' + basicGame.player1Pos);
+			elemToUpdate.className += ' player1Pressed ';
+		} else if (e.keyCode == 50) {
+			basicGame.player2Pos += 1;
+			elemToUpdate = document.getElementById('player2-' + basicGame.player2Pos);
+			elemToUpdate.className += ' player2Pressed ';
+		}
+
+		// Did anybody win?
+		if (basicGame.player1Pos == basicGame.winningScore) {
+			alert('Player 1 Wins!');
+		} else if (basicGame.player2Pos == basicGame.winningScore) {
+			alert('Player 2 Wins!');
+		}
+
+	}
+};
+
+window.onload = function() {
+	basicGame.init();
+}
